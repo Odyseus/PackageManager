145fcfb88c82a6cac6efef4ca1401a2e9016712a
Odyseus <venusyadonis@gmail.com>
2018-09-15 01:25:56 -0300

    Bump version.

--- AppData/PackageManagerApp/__init__.py
+++ AppData/PackageManagerApp/__init__.py
@@ -4,4 +4,4 @@
 __status__ = "(Unstable)"
 __appname__ = "Package Manager"
 __appdescription__ = "A CLI utility to manage system packages on GNU/Linux. It basically installs and/or eliminates packages in bulk using the system's package manager."
-__version__ = "2018-06-12 02:20:15.237"
+__version__ = "2018-09-15 01:19:13.336"
