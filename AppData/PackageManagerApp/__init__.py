5473966a4a71ca57da853f572f7f938673862350
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:39:29 -0300

    Added README and updated __init__.py module.

--- AppData/PackageManagerApp/__init__.py
+++ AppData/PackageManagerApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Unstable)"
 __appname__ = "Package Manager"
 __appdescription__ = "A CLI utility to manage system packages on GNU/Linux. It basically installs and/or eliminates packages in bulk using the system's package manager."
 __version__ = "2018-09-15 01:19:13.336"
+
+
+if __name__ == "__main__":
+    pass
