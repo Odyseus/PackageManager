06238aab77f92b53d846f3315f29911f1aac4173
Odyseus <venusyadonis@gmail.com>
2020-03-20 21:49:38 -0300

    Updated manual page and bump version

--- AppData/PackageManagerApp/__init__.py
+++ AppData/PackageManagerApp/__init__.py
@@ -10,4 +10,4 @@ __appname__ = "Package Manager"
 
 __appdescription__ = "CLI utility to manage system packages on GNU/Linux. It basically installs and/or eliminates packages in bulk using the system's package manager."
 
-__version__ = "2020-03-08 09:10:52.474"
+__version__ = "2020-03-20 21:47:57.129"
